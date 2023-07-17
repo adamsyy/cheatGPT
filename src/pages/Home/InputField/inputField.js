5481edf3f2d154a143759325e921770a7da08c22
Jaison Dennis <jaisondennis080@gmail.com>
2023-07-17 15:40:24 +0530

    Made some reponsive fixes

--- src/pages/Home/InputField/inputField.js
+++ src/pages/Home/InputField/inputField.js
@@ -1,63 +1,59 @@
-import React, { useState, useRef } from 'react';
-import styles from './inputField.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useRef } from "react";
+import styles from "./inputField.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function InputField({ setResultData }) {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [isLoading, setIsLoading] = useState(false); // Add isLoading state
-  const inputRef = useRef(null);
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-    inputRef.current.style.height = 'auto';
-    inputRef.current.style.height = `${inputRef.current.scrollHeight}px`;
-  };
 
   const handleSubmit = () => {
     const requestBody = { phrase: value };
     setIsLoading(true); // Set isLoading to true when submitting
 
-    fetch('https://flask-hello-world-theta-green.vercel.app/paraphrase1', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(requestBody)
+    fetch("https://flask-hello-world-theta-green.vercel.app/paraphrase1", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         setTimeout(() => {
           setResultData(data.paraphrases);
           setIsLoading(false); // Set isLoading to false after receiving the results
         }, 0);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
         setIsLoading(false); // Set isLoading to false on error
       });
   };
 
   return (
-    <div>
-    <div className={styles.inputFieldContainer}>
-      <textarea
-        placeholder="Enter your text here"
-        value={value}
-        onChange={handleChange}
-        className={styles.inputField}
-        ref={inputRef}
-        style={{ fontFamily: "San Francisco" }}
-      />
-      <FontAwesomeIcon
-        icon={faCheck}
-        color="black"
-        className={styles.tickicon}
-        size="10x"
-        onClick={handleSubmit}
-      />
-    </div>     {isLoading && <div className={styles.loader} /> /* Render the loader when isLoading is true */}
-    
-    </div>
+    <>
+      <div className={styles.inputFieldContainer}>
+        <textarea
+          placeholder="Enter your text here"
+          value={value}
+          rows={value.split("\n").length < 3 ? 3 : value.split("\n").length + 2}
+          onChange={(e) => setValue(e.target.value)}
+          style={{ fontFamily: "San Francisco" }}
+        />
+        <FontAwesomeIcon
+          icon={faCheck}
+          color="black"
+          className={styles.tickicon}
+          size="10x"
+          onClick={handleSubmit}
+        />
+      </div>{" "}
+      {
+        isLoading && (
+          <div className={styles.loader} />
+        ) /* Render the loader when isLoading is true */
+      }
+    </>
   );
 }
 
--- src/pages/Home/Navbar/Navbar.js
+++ src/pages/Home/Navbar/Navbar.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import styles from './Navbar.module.css';
-import logo from './logo.png';
+import React from "react";
+import styles from "./Navbar.module.css";
+import logo from "./logo.png";
 
 const Navbar = () => {
   const handleContributeClick = () => {
-    window.open('https://www.buymeacoffee.com/adamsyyy', '_blank');
+    window.open("https://www.buymeacoffee.com/adamsyyy", "_blank");
   };
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-      </div>
-      <ul className={styles.navItems}>
-        <li className={styles.navItem} onClick={handleContributeClick}>
+    <div className={styles.navbar}>
+      <img src={logo} alt="Logo" className={styles.logo} />
+      <div className={styles.navItems}>
+        <div className={styles.navItem} onClick={handleContributeClick}>
           Contribute
-        </li>
-      </ul>
-    </nav>
+        </div>
+      </div>
+    </div>
   );
 };
 
